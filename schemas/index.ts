b197e00001a743c2c103ba44647875b866004a44
naveen <naveenmk675@gmail.com>
2024-01-23 02:47:41 +0530

    register page created

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,9 +2,22 @@ import * as z from "zod"
 
 export const LoginSchema=z.object({
     email:z.string().email({
-        message:"email is required"
+        message:"Email is required"
     }),
     password:z.string().min(1,{
         message:"Password is required"
+    })
+})
+
+
+export const RegisteSchema=z.object({
+    email:z.string().email({
+        message:"Email is required"
+    }),
+    password:z.string().min(6,{
+        message:"Minimum 6 characters is required"
+    }),
+    name:z.string().min(1,{
+        message:"Name is required"
     })
 })
